33fbc9a7cdf72e49a85a6c1290aa48aa92531913
antcc <antoniocoin07@gmail.com>
2018-04-14 13:21:44 +0200

    Merge

--- P2/Comportamientos_Jugador/jugador.cpp
+++ P2/Comportamientos_Jugador/jugador.cpp
@@ -3,40 +3,217 @@
 
 #include <iostream>
 #include <cmath>
+#include <queue>
+#include <vector>
 
-void ComportamientoJugador::PintaPlan(list<Action> plan) {
-	auto it = plan.begin();
-	while (it!=plan.end()){
-		if (*it == actFORWARD){
+using namespace std;
+
+/***************** FUNCIONES AUXILIARES ******************/
+
+namespace {
+
+Orientation operator+(const Action& act, const Orientation& ori) {
+  Orientation new_ori;
+  int move;
+
+  if (act == actTURN_L)
+    move = -1;
+  else if (act == actTURN_R)
+    move = 1;
+
+  switch(act) {
+    case actFORWARD:
+    case actIDLE:
+      new_ori = ori;
+      break;
+    case actTURN_L:
+    case actTURN_R:
+      new_ori = static_cast<Orientation>((ori + move + 4) % 4);
+      break;
+  }
+  return new_ori;
+}
+
+int f(node* n) {
+  return n->h + n->g;
+}
+
+int manhattan_distance(const estado& origen, const estado& destino) {
+  return abs(origen.fila - destino.fila) + abs(origen.columna - destino.columna);
+}
+
+node * mknode(const estado& pos, const estado& destino, node * parent) {
+  int parent_g;
+  if (parent)
+    parent_g = parent->g;
+  else
+    parent_g = -1;
+
+  node* n = new node(parent, parent_g, pos);
+  n->h = manhattan_distance(pos, destino);
+  return n;
+}
+
+void delete_list(list<node*> l) {
+  for (auto it = l.begin(); it != l.end(); ++it)
+    delete *it;
+}
+
+void reconstruct_path(stack<Action>& plan, node * destination) {
+  node* current = destination;
+
+  while (current) {
+    stack<Action> partial_path = current->actions;
+    while (!partial_path.empty()) {
+      plan.push(partial_path.top());
+      partial_path.pop();
+    }
+    current = current->parent;
+  }
+}
+
+}
+
+/***************** MÉTODOS DE LA CLASE *******************/
+
+void ComportamientoJugador::PintaPlan(stack<Action> plan) {
+	while (!plan.empty()){
+    Action act = plan.top();
+		if (act == actFORWARD){
 			cout << "A ";
 		}
-		else if (*it == actTURN_R){
+		else if (act == actTURN_R){
 			cout << "D ";
 		}
-		else if (*it == actTURN_L){
+		else if (act == actTURN_L){
 			cout << "I ";
 		}
 		else {
 			cout << "- ";
 		}
-		it++;
+		plan.pop();
 	}
 	cout << endl;
 }
 
-bool ComportamientoJugador::pathFinding(const estado &origen, const estado &destino,
-                                        list<Action> &plan) {
-  return false;
+void ComportamientoJugador::actualizaPos(estado& pos, Action next) {
+  if (next == actFORWARD) {
+    switch(pos.orientacion) {
+      case NORTH:
+        pos.fila--;
+        break;
+      case SOUTH:
+        pos.fila++;
+        break;
+      case EAST:
+        pos.columna++;
+        break;
+      case WEST:
+        pos.columna--;
+        break;
+    }
+  }
+  pos.orientacion = next + pos.orientacion;
+}
+
+/**
+ * A* algorithm
+ */
+bool ComportamientoJugador::pathFinding(const estado& origen,
+                                        const estado& destino,
+                                        stack<Action>& plan) {
+  // Custom comparator for nodes
+  struct comp_nodes {
+    bool operator()(node * n1, node * n2) {
+      return f(n1) < f(n2);
+    }
+  };
+
+  bool found = false;
+  priority_queue<node*, vector<node*>, comp_nodes> open_set;
+  list<node*> closed_set;
+
+  // Start node
+  open_set.push(mknode(origen, destino, NULL));
+
+
+  // if(llegado a destino) found=true
+
+
+
+  // Generate actual path
+  reconstruct_path(plan, closed_set.front());
+
+  // Free memory
+  while (!open_set.empty()) {
+    delete open_set.top();
+    open_set.pop();
+  }
+  delete_list(closed_set);
+
+  return found;
 }
 
 Action ComportamientoJugador::think(Sensores sensores) {
-  /*if (plan.empty()) {
-    pathFinding({fil, col, brujula}, destino, plan);
+  /**
+   * Asumo que el juego se detiene al llegar al destino.
+   *
+   */
+  Action next_move;
+
+  if (pasos == 0) {
+    pos.fila = sensores.mensajeF;
+    pos.columna = sensores.mensajeC;
+    destino.fila = sensores.destinoF;
+    destino.columna = sensores.destinoC;
   }
-  else {
-    return plan.at(next_move++);
-  }*/
-  return actFORWARD;
+
+  // Plan itinerary the first time
+  if (plan.empty()) {
+    if (!pathFinding(pos, destino, plan))
+      cout << "Error: no hay camino posible al destino." << endl;
+  }
+
+  //next_move = plan.top();
+  //plan.pop();
+
+  actualizaPos(pos, next_move);
+  pasos++;
+
+  return actIDLE;
+}
+
+void AnularMatriz(vector<vector<unsigned char> > &m){
+	for (int i=0; i<m[0].size(); i++){
+		for (int j=0; j<m.size(); j++){
+			m[i][j]=0;
+		}
+	}
+}
+
+void ComportamientoJugador::VisualizaPlan(const estado &st, const list<Action> &plan){
+  AnularMatriz(mapaConPlan);
+	estado cst = st;
+
+	auto it = plan.begin();
+	while (it!=plan.end()){
+		if (*it == actFORWARD){
+			switch (cst.orientacion) {
+				case 0: cst.fila--; break;
+				case 1: cst.columna++; break;
+				case 2: cst.fila++; break;
+				case 3: cst.columna--; break;
+			}
+			mapaConPlan[cst.fila][cst.columna]=1;
+		}
+		else if (*it == actTURN_R){
+			cst.orientacion = (cst.orientacion+1)%4;
+		}
+		else {
+			cst.orientacion = (cst.orientacion+3)%4;
+		}
+		it++;
+	}
 }
 
 int ComportamientoJugador::interact(Action accion, int valor) {
--- P2/Comportamientos_Jugador/jugador.hpp
+++ P2/Comportamientos_Jugador/jugador.hpp
@@ -2,51 +2,71 @@
 #define COMPORTAMIENTOJUGADOR_H
 
 #include "comportamientos/comportamiento.hpp"
-
+#include <stack>
 #include <list>
+
+// 0=Norte, 1=Este, 2=Sur, 3=Oeste
+enum Orientation {NORTH, EAST, SOUTH, WEST};
 
 struct estado {
   int fila;
   int columna;
-  int orientacion;
+  Orientation orientacion;
+
+  bool operator==(const estado& otro) {
+    return fila == otro.fila && columna == otro.columna;
+  }
+};
+
+struct node {
+  int h;
+  int g;
+  estado pos;
+  stack<Action> actions; // Actions performed to get from parent to this node
+  node* parent;
+
+  node(node * parent, int g_parent, const estado& pos) {
+    this->parent = parent;
+    g = g_parent + 1;
+    this->pos = pos;
+  }
 };
 
 class ComportamientoJugador : public Comportamiento {
   public:
-    ComportamientoJugador(unsigned int size) : Comportamiento(size) {
+
+    void constructor() {
       // Inicializar Variables de Estado
-      fil = col = 99;
-      brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
+      pos.fila = pos.columna = 99;
+      pos.orientacion = NORTH;
       destino.fila = -1;
       destino.columna = -1;
-      destino.orientacion = -1;
-      next_move = 0;
+      destino.orientacion = NORTH; // Indiferente
+      pasos = 0;
+    }
+
+    ComportamientoJugador(unsigned int size) : Comportamiento(size) {
+      constructor();
     }
     ComportamientoJugador(std::vector< std::vector< unsigned char> > mapaR) : Comportamiento(mapaR) {
-      // Inicializar Variables de Estado
-      fil = col = 99;
-      brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
-      destino.fila = -1;
-      destino.columna = -1;
-      destino.orientacion = -1;
-      next_move = 0;
+      constructor();
     }
-    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
-    ~ComportamientoJugador(){}
+    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport) {}
+    ~ComportamientoJugador() {}
 
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
-    ComportamientoJugador * clone(){return new ComportamientoJugador(*this);}
+    ComportamientoJugador * clone() {return new ComportamientoJugador(*this);}
 
   private:
     // Declarar Variables de Estado
-    int fil, col, brujula;
-    int next_move;
-    estado destino;
-    list<Action> plan;
+    estado pos, destino;
+    int pasos;
+    stack<Action> plan;
 
-    bool pathFinding(const estado &origen, const estado &destino, list<Action> &plan);
-    void PintaPlan(list<Action> plan);
+    void actualizaPos(estado& pos, Action next);
+    bool pathFinding(const estado &origen, const estado &destino, stack<Action> &plan);
+    void PintaPlan(stack<Action> plan);
 };
 
 #endif
