e4930808c4c6b1c8a7460287b88da6ae7f93599e
antcc <antoniocoin07@gmail.com>
2018-05-17 23:29:45 +0200

    Implementación inicial completada. Sin memoria. Gana a greedyBot

--- P3/MancalaEngine/GriffinBot.cpp
+++ P3/MancalaEngine/GriffinBot.cpp
@@ -11,12 +11,169 @@
 
 using namespace std;
 
+/*****
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+* EN EL CUTOFF-TEST HAY QUE TENER EN CUENTA
+  EL TIEMPO MÁXIMO Y LOS NODOS quiescent.  Consider storing leaf and quiescence nodes in the table.
+
+* Tabla hash
+
+* Tener en cuenta en la heurística los turnos extra
+
+* Revisar heurística (J1, J2, modifier)
+* En la raíz, maximizing = true??
+
+* Iterative deepening? considerar guardar el orden de los nodos para no tener que volver
+* a ordenar los hijos ya explorados.
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+*******/
+
+namespace {
+
+  /**
+   * Delete a NodeQueue
+   *
+   */
+  void deleteQueue(NodeQueue& q) {
+    while (!q.empty()) {
+      auto value = q.top();
+      q.pop();
+      delete value;
+    }
+  }
+
+  /**
+   * Heuristic function to evaluate the score of a board
+   *
+   */
+  int heuristic(Node * n) {
+    return n->board.getScore(J1) - n->board.getScore(J2);
+  }
+
+  /**
+   * Implementation of memory-enhanced alpha-beta pruning
+   *
+   */
+  pair<Bound, Move> alphaBetaWithMemory(Node * node, int depth, int alpha, int beta) {
+    if (depth == 0 || node->board.isFinalState())
+      return make_pair(node->h_value, M_NONE);
+
+    Bound best_bound;
+    Move best_move;
+    NodeQueue children = node->children();
+
+    if (node->maximizing_player) {
+      best_bound = - (1 << 10);  // - infinity
+
+      while (!children.empty()) {
+        auto child = children.top();
+        auto child_bound = alphaBetaWithMemory(child, depth - 1, alpha, beta).first;
+        children.pop();
+
+        if (child_bound > best_bound) {
+          best_bound = child_bound;
+          best_move = child->prev_move;
+        }
+
+        alpha = max(alpha, best_bound);
+
+        // DELETE PROVISIONAL
+        delete child;
+
+        if (beta <= alpha) {
+          deleteQueue(children);
+          break;
+        }
+      }
+
+      return make_pair(best_bound, best_move);
+    }
+
+    else {  // minimizing_player
+      best_bound = 1 << 10;  // + infinity
+
+      while (!children.empty()) {
+        auto child = children.top();
+        auto child_bound = alphaBetaWithMemory(child, depth - 1, alpha, beta).first;
+        children.pop();
+
+        if (child_bound < best_bound) {
+          best_bound = child_bound;
+          best_move = child->prev_move;
+        }
+
+        beta = min(beta, best_bound);
+
+        // DELETE PROVISIONAL
+        delete child;
+
+        if (beta <= alpha) {
+          deleteQueue(children);
+          break;
+        }
+      }
+
+      return make_pair(best_bound, best_move);
+    }
+  }
+
+  /**
+   * Implementation of MTD-f search algorithm
+   *
+   */
+  Move mtdf(Node * root, Bound first_guess, int depth) {
+    pair<Bound, Move> best_action;
+    Bound best_bound = first_guess;
+    int upper_bound = 1 << 10;   // + infinity
+    int lower_bound = - (1 << 10);  // - infinity
+
+    while (lower_bound < upper_bound) {
+      int beta = max(best_bound, lower_bound + 1);
+      best_action = alphaBetaWithMemory(root, depth, beta - 1, beta);
+      best_bound = best_action.first;
+
+      // Update current bounds
+      if (best_bound < beta)
+        upper_bound = best_bound;
+      else
+        lower_bound = best_bound;
+    }
+
+    return best_action.second;
+  }
+
+}
+
+Node::Node(const GameState& board, Move prev_move, bool maximizing_player) {
+  this->board = board;
+  this->prev_move = prev_move;
+  this->maximizing_player = maximizing_player;
+  this->h_value = heuristic(this);
+}
+
+NodeQueue Node::children() {
+  NodeQueue q;
+  for (int i = 1; i <= 6; i++) {
+    GameState new_board = board.simulateMove((Move) i);
+    bool maximizing = new_board.getCurrentPlayer() == board.getCurrentPlayer()
+                      ? maximizing_player : !maximizing_player;
+    Node* node = new Node(new_board, (Move) i, maximizing);
+    q.push(node);
+  }
+
+  return q;
+}
+
 GriffinBot::GriffinBot() {
 	// Inicializar las variables necesarias para ejecutar la partida
 }
 
 GriffinBot::~GriffinBot() {
-	// Liberar los recursos reservados (memoria, ficheros, etc.)
+	// Liberar los recursos reservados
 }
 
 void GriffinBot::initialize() {
@@ -24,12 +181,15 @@ void GriffinBot::initialize() {
 }
 
 string GriffinBot::getName() {
-	return "GriffinBot"; // Sustituir por el nombre del bot
+	return "GriffinBot";
 }
 
 Move GriffinBot::nextMove(const vector<Move>& adversary, const GameState& state) {
+  Move next_move;
+  Node* root = new Node(state, M_NONE, getPlayer() == J1);
 
-	Move movimiento = M_NONE;
+  next_move = mtdf(root, 0, 10);
+  delete root;
 
-	return movimiento;
+	return next_move;
 }
--- P3/MancalaEngine/GriffinBot.h
+++ P3/MancalaEngine/GriffinBot.h
@@ -5,18 +5,42 @@
  */
 
 #include "Bot.h"
+#include <queue>
+#include <vector>
 
 #ifndef GRIFFINBOT_H_
 #define GRIFFINBOT_H_
 
-class GriffinBot : Bot {
-public:
-	GriffinBot();
-	~GriffinBot();
+struct Node;
+struct NodeComp;
 
-	void initialize();
-	string getName();
-	Move nextMove(const vector<Move>& adversary, const GameState& state);
+using Bound = int;
+using NodeQueue = std::priority_queue<Node*, std::vector<Node*>, NodeComp>;
+
+struct Node {
+  GameState board;
+  Move prev_move;  // Movement performed to get to this node
+  bool maximizing_player;
+  int h_value;
+
+  Node(const GameState& board, Move prev_move, bool maximizing_player);
+  NodeQueue children();
+};
+
+struct NodeComp {
+  bool operator()(Node * n1, Node * n2) {
+    return n1->h_value > n2->h_value;
+  }
+};
+
+class GriffinBot : Bot {
+  public:
+    GriffinBot();
+    ~GriffinBot();
+
+    void initialize();
+	  string getName();
+	  Move nextMove(const vector<Move>& adversary, const GameState& state);
 };
 
 #endif /* GRIFFINBOT_H_ */
